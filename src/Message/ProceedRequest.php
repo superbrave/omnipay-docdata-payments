fa30fb456462221bb36cf75ed12ae27c6d783d62
Krijn <kvb@superbrave.nl>
2019-01-23 11:00:36 +0100

    Add support for Proceed call with bank transfer payment method (so the flow of the payments still remains CREATE > START > (return) > PROCEED > CAPTURE, for every payment method.

--- src/Message/ProceedRequest.php
+++ src/Message/ProceedRequest.php
@@ -50,26 +50,53 @@ class ProceedRequest extends SoapAbstractRequest
         }
 
         $lastProceedResult = null;
+        $authorizedPayments = [];
+
         foreach($payments as $payment) {
+            if (isset($payment->authorization->reversal)) {
+                continue;
+            }
+
             // try to 'proceed' every payment that has a valid state.
             // states are, however, badly documented.
-            if (in_array($payment->authorization->status,[
-                'REDIRECTED_FOR_AUTHENTICATION',
-                'AUTHORIZATION_REQUESTED',
-                'RISK_CHECK_OK'
-            ])) {
-                $data['paymentId'] = $payment->id;
+            switch($payment->authorization->status) {
+                case 'REDIRECTED_FOR_AUTHENTICATION':
+                case 'AUTHORIZATION_REQUESTED':
+                case 'RISK_CHECK_OK':
+                    // we can proceed
+                    $data['paymentId'] = $payment->id;
+    
+                    if (!empty($this->getAuthorizationResultType())) {
+                        $data[$this->getAuthorizationResultType()] = $this->getAuthorizationResult();
+                    }
 
-                if (!empty($this->getAuthorizationResultType())) {
-                    $data[$this->getAuthorizationResultType()] = $this->getAuthorizationResult();
-                }
-
-                $lastProceedResult = $soapClient->__soapCall('proceed', [$data]);
+                    // we can't return here because there might be multiple payments that need to proceed
+                    $lastProceedResult = $soapClient->__soapCall('proceed', [$data]);
+                    break;
+                case 'AUTHORIZED':
+                    if (
+                        isset($payment->authorization->capture->status)
+                        && $payment->authorization->capture->status === 'STARTED'
+                    ) {
+                        $authorizedPayments[] = $payment;
+                    }
+                    break;
             }
         }
-        // even if we were to have multiple payments, the last one should be the most relevant
-        // and should have the result returned
-        // @todo return 'fake' success response if no proceed is required, or failed state otherwise
+
+        if ($lastProceedResult === null) {
+            if (!empty($this->getAuthorizationResultType())) {
+                // we should have proceeded but we can't
+                return $this->createFakeProceedResponseForNoValidPayments();
+            }
+
+            if (!empty($authorizedPayments)) {
+                // bank transfer. Promise to pay, but no money yet.
+                return $this->createSuccessfulProceedResponseForValidPaymentsButNothingToDo();
+            }
+        }
+        // Even if we were to have multiple payments, the last one should be the most relevant
+        // and should have the result returned.
         return $lastProceedResult;
     }
 
@@ -84,9 +111,9 @@ class ProceedRequest extends SoapAbstractRequest
     }
 
     /**
-     * @return string
+     * @return string|null
      */
-    public function getAuthorizationResultType(): string
+    public function getAuthorizationResultType()
     {
         return $this->authorizationResultType;
     }
@@ -113,5 +140,42 @@ class ProceedRequest extends SoapAbstractRequest
     public function setAuthorizationResult(array $authorizationResult)
     {
         $this->authorizationResult = $authorizationResult;
+    }
+
+
+    /**
+     * Create a stdClass that mimics a failed soap call to docdata, to be used instead of an exception
+     *
+     * @return \stdClass
+     */
+    private function createFakeProceedResponseForNoValidPayments()
+    {
+        $response = new \stdClass();
+        $response->proceedErrors = new \stdClass();
+        $response->proceedErrors->error = new \stdClass();
+        $response->proceedErrors->error->_ = 'No Proceed executed because there were no valid payments';
+
+        return $response;
+    }
+
+    /**
+     * Create a stdClass that mimics a successful soap call to docdata.
+     * This is used when there were no (authorized) payments to proceed.
+     * This occurs when a user chooses bank transfer.
+     *
+     * @return \stdClass
+     */
+    private function createSuccessfulProceedResponseForValidPaymentsButNothingToDo()
+    {
+        $response = new \stdClass();
+        $response->proceedSuccess = new \stdClass();
+        $response->proceedSuccess->success = new \stdClass();
+        $response->proceedSuccess->success->_ = 'All payments were already processed so no action was taken.';
+        $response->proceedSuccess->success->code = 'SUCCESS';
+        $response->proceedSuccess->paymentResponse = new \stdClass();
+        $response->proceedSuccess->paymentResponse->paymentSuccess = new \stdClass();
+        $response->proceedSuccess->paymentResponse->paymentSuccess->status = 'AUTHORIZED';
+
+        return $response;
     }
 }
